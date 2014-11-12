b76fed29fd52f2e5fefd38726f3db92f03b54409
BenjaminCranwell <benjamin.cranwell@yahoo.co.uk>
2014-11-12 15:33:54 +0000

    ominauth heroku completed plus b\ookmarked

--- app/assets/javascripts/bookmark.js
+++ app/assets/javascripts/bookmark.js
@@ -1,9 +1,29 @@
 
-function bookmark(){
-  $this = $(this)
-  articleId = $this.data("id");
-  isDone = $this.is(":checked")
-  request("POST", "/taskb/"+taskId, {task:{done: isDone}}).success(function(){
-    $this.parent().toggleClass("completed")
+function request(method, url, data){
+  return $.ajax({
+    method: method,
+    url: url,
+    dataType: "json",
+    data: data,
   })
 }
+
+function bookmark(){
+  $this = $(this)
+  $articleId = $this.data("id");
+  console.log($articleId)
+  // isDone = $this.is(":checked")
+  request("POST", "/bookmarks", {
+    bookmark:{
+    article_id: $articleId,
+  }
+  }).success(function(data){
+    console.log(data)
+    // append .unbookmark
+  })
+}
+
+$(function(){
+  $('body').on('click', '.bookmark-article', bookmark);
+  // $('body').on('click', '.unbookmarked', unBookmark);
+})
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :article_id, :user_id
+  attr_accessible :article_id, :user_id, :is_bookmarked, :is_favourited
 
   belongs_to :user
   belongs_to :article
