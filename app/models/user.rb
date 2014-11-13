553c6a19e83ab88aac5d8b1ac03e8d8d424683bf
BenjaminCranwell <benjamin.cranwell@yahoo.co.uk>
2014-11-13 17:30:34 +0000

    no letter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :omniauthable, :omniauth_providers => [:google_oauth2, :twitter, :facebook]
 
   # Setup accessible (or protected) attributes for your model
