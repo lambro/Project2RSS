d63273380c0fe59fd797740c12324613a9ab263a
Lamb <lauraambrose@gmail.com>
2014-11-13 15:31:08 +0000

    finished working on the feeds show page, I should probably switch branch!

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :article_id, :user_id, :is_bookmarked, :is_favourited
+  attr_accessible :article_id, :user_id, :is_bookmarked, :is_favourited, :article, :user
 
   belongs_to :user
   belongs_to :article
@@ -9,5 +9,13 @@ class Bookmark < ActiveRecord::Base
       Article.find(x.id).title
     end  
   end
+
+# def bookmark_show_title(bookmark)
+
+#   @bookmark_show_title = bookmark.map do |x|
+#     Article.find(x).title
+#   end
+  
+# end
 
 end
