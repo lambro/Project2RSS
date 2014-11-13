d7925fdde6cf084344ba8899ed1450bf363ffcd4
Lamb <lauraambrose@gmail.com>
2014-11-13 23:27:16 +0000

    added css to subscriptions and links to category_show

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :description, :picture, :title
+  attr_accessible :description, :picture, :title, :feed
 
   has_many :feeds
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,27 @@ c3 = Category.create!(title: "Tech", description: "Up to date tech news", pictur
 c4 = Category.create!(title: "Politics", description: "Current politics from around the world", picture: "politics.jpg")
 c5 = Category.create!(title: "Entertainment", description: "Latest entertainment news", picture: "entertainment.jpg")
 c6 = Category.create!(title: "Fashion", description: "Latest fashion news", picture: "fashion.jpg")
-c7 = Category.create!(title: "Science", description: "Latest Science news", picture: "fashion.jpg")
-c8 = Category.create!(title: "Health", description: "Latest fashion news", picture: "fashion.jpg")
+c7 = Category.create!(title: "Science", description: "Latest Science news", picture: "science.jpg")
+c8 = Category.create!(title: "Health", description: "Latest fashion news", picture: "health.jpg")
+c8 = Category.create!(title: "Business", description: "Latest fashion news", picture: "business.jpg")
 
 
 f1 = Feed.create!(url:"http://feeds.bbci.co.uk/news/world/rss.xml",title:"BBC", category_id: c1.id)
+f2 = Feed.create!(url:"http://www.telegraph.co.uk/news/uknews/rss",title:"The Telegraph", category_id: c1.id)
 f3 = Feed.create!(url:"http://www.theguardian.com/world/rss",title:"The Guardian", category_id:c1.id)
 f4 = Feed.create!(url:"http://feeds.feedburner.com/fubiz",title:"Fubiz",category_id:c5.id)
+f5 = Feed.create!(url:"http://www.instyle.co.uk/fashion/news.rss",title:"Fashion",category_id:c6.id)
 f6 = Feed.create!(url:"http://www.skysports.com/rss/0,20514,11095,00.xml",title:"Sky Sports",category_id:c2.id)
-f7 = Feed.create!(url:"http://news.nationalgeographic.com/index.rss",title:"National Geographic",category_id:c4.id)
+f7 = Feed.create!(url:"http://news.nationalgeographic.com/index.rss",title:"National Geographic",category_id:c7.id)
+f8 = Feed.create!(url:"http://rss.feedsportal.com/c/266/f/3803/index.rss",title:"The Independent",category_id:c6.id)
 f9 = Feed.create!(url:"http://www.npr.org/rss/rss.php?id=1001",title:"NPR",category_id:c1.id)
 f10 = Feed.create!(url:"http://feeds.gawker.com/lifehacker/full.xml",title:"Lifehacker",category_id:c3.id)
+f11 = Feed.create!(url:"http://www.fashiontv.com/rss/designers",title:"Fashion TV",category_id:c6.id)
+f12 = Feed.create!(url:"http://www.theguardian.com/uk/sport/rss",title:"The Guardian",category_id:c2.id)
+f13 = Feed.create!(url:"http://sports.yahoo.com/soccer//rss.xml",title:"Yahoo Sports",category_id:c2.id)
+
+
+
+
+
+
