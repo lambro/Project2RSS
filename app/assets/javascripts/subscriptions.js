692e4505e84c67f6b5e8f5687fe5ee0b4f46fc94
BenjaminCranwell <benjamin.cranwell@yahoo.co.uk>
2014-11-13 10:42:03 +0000

    working subscribtion

---
+++ app/assets/javascripts/subscriptions.js
@@ -0,0 +1,39 @@
+
+function request(method, url, data){
+  return $.ajax({
+    method: method,
+    url: url,
+    dataType: "json",
+    data: data,
+  })
+}
+
+function subscribe(){
+  $this = $(this)
+  $feedId = $this.data("id");
+  request("POST", "/subscriptions", {subscription:{feed_id:$feedId}}).success(function(data){
+    console.log(data.id)
+    $this.removeClass('subscription-feed')
+    $this.addClass('subscribed')
+    $this.data('id', data.id)
+  })
+}
+
+function unSubscribe(){
+  $this = $(this)
+  $subsId = $this.data("id");
+  console.log($subsId)
+  request("DELETE", "/subscriptions/"+ $subsId, null).success(function(data){
+
+    $this.removeClass('subscribed')
+    $this.addClass('subscription-feed')
+    console.log('hello')
+})
+}
+   
+
+$(function(){
+  $('body').on('click', '.subscription-feed', subscribe);
+  $('body').on('click', '.subscribed', unSubscribe);
+
+})
--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,9 @@
 module SubscriptionsHelper
+  # def user_subscribed?(feed)
+  #   feed.subscriptions.map{|subs| subs.user_id}.include?(current_user.id)
+  # end
+
+  # def subscription_id(feed)
+  #   feed.subscriptions.where(user_id: current_user.id).first.id
+  # end
 end
